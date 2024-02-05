37bf1acab0fd484e14f4fab18f56b3758c011fed
Gabriel Chagas <gabrielserq@outlook.com>
2024-02-05 01:41:28 -0300

    chore: import components

--- 02_fundamentos/fundamentos/src/App.jsx
+++ 02_fundamentos/fundamentos/src/App.jsx
@@ -2,3 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+// Components
+import FirstComponent from './components/FirstComponent'
+
+import './App.css'
+import Challenge from './components/Challenge'
