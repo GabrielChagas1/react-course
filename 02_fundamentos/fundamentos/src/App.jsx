27b212000edb81e7e6e94ffb314dcafc2c577dcf
Gabriel Chagas <gabrielserq@outlook.com>
2024-02-06 01:41:50 -0300

    chore: adding app.jsx

--- 02_fundamentos/fundamentos/src/App.jsx
+++ 02_fundamentos/fundamentos/src/App.jsx
@@ -7,3 +7,33 @@ import FirstComponent from './components/FirstComponent'
 
 import './App.css'
 import Challenge from './components/Challenge'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <FirstComponent />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <Challenge />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
