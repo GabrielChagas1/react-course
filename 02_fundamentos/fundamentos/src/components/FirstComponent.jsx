ee08ea83c69d3ea0c3da3824432fa88825d63d95
Gabriel Chagas <gabrielserq@outlook.com>
2024-02-10 01:48:12 -0300

    chore: add FirstComponent.jsx

--- 02_fundamentos/fundamentos/src/components/FirstComponent.jsx
+++ 02_fundamentos/fundamentos/src/components/FirstComponent.jsx
@@ -1,2 +1,15 @@
 import { TemplateExpressions } from '../components/TemplateExpressions'
 import Events from './Events'
+
+const FirstComponent = () => {
+
+  return (
+    <>
+      <div>First Component</div>
+      <TemplateExpressions />
+      <Events />
+    </>
+  )
+}
+
+export default FirstComponent
