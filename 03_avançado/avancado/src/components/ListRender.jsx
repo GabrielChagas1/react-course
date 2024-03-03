817203979988de64aaef4cd7848b7c73d123cf5c
Gabriel Chagas <gabrielserq@outlook.com>
2024-03-03 20:31:26 -0300

    chore: creating variable users

--- 03_avançado/avancado/src/components/ListRender.jsx
+++ 03_avançado/avancado/src/components/ListRender.jsx
@@ -1,1 +1,8 @@
 import { useState } from 'react'
+
+    const [users, setUsers] = useState([
+      {id: 1, name: 'Gabriel', age: 31},
+      {id: 2, name: 'Gabriel', age: 31},
+      {id: 3, name: 'Gabriel', age: 31}
+    ])
+
