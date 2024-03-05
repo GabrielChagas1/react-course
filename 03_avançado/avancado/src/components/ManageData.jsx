bbd85f887bee119340f6793384f6bcfaf9d4c8ea
Gabriel Chagas <gabrielserq@outlook.com>
2024-03-05 20:33:29 -0300

    chore: creating function handeGetNumber

--- 03_avançado/avancado/src/components/ManageData.jsx
+++ 03_avançado/avancado/src/components/ManageData.jsx
@@ -2,3 +2,7 @@ import React, { useState } from 'react'
 import ShowUserName from './showUserName';
 
     const someData = 10;
+
+    function handleGetNumber(){
+      setNumber(document.getElementsByClassName('name')[0].value)
+    }
