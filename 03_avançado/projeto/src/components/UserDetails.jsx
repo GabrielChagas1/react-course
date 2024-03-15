4600467e2521d154a775082858b4afbf320e5d47
Gabriel Chagas <gabrielserq@outlook.com>
2024-03-15 20:45:20 -0300

    chore: creating userDetails

--- 03_avançado/projeto/src/components/UserDetails.jsx
+++ 03_avançado/projeto/src/components/UserDetails.jsx
@@ -1,1 +1,18 @@
 import '../index.css'
+
+const UserDetails = ({name, age, profession}) => {
+  return (
+    <div>
+        {
+            <div className="card">
+                <p>name: {name}</p>
+                <p>age: {age}</p>
+                <p>profession: {profession}</p>
+                {age >= 18 ? <p>Carteira de Habilitação: ✅</p> : <p>Carteira de Habilitação: ❌</p>}
+            </div>
+                       
+        }
+    </div>
+  )
+}
+
