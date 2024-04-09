1b069799a58e5551684628108c7f03e5f53de545
Gabriel Chagas <gabrielserq@outlook.com>
2024-04-09 20:57:27 -0300

    chore: creating const cars

--- 04_css_react/challengecss/src/App.jsx
+++ 04_css_react/challengecss/src/App.jsx
@@ -3,3 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CarDetails from './Components/CarDetails'
+  const [cars, setCar] = useState([
+    {
+      id: 1,
+      name: 'Gol',
+      color: 'Azul',
+      value: '15.000',
+      available: false
+    },
+    {
+      id: 2,
+      name: 'Fiesta',
+      color: 'Branco',
+      value: '20.000',
+      available: true
+    },
+    {
+      id: 3,
+      name: 'Corsa',
+      color: 'Preto',
+      value: '12.000',
+      available: true
+    }
+  ])
