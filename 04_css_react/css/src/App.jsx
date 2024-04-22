c627d679a11f4bb01071b34faf374bf8293e38bd
Gabriel Chagas <gabrielserq@outlook.com>
2024-04-21 21:07:51 -0300

    chore: add App.jsx

--- 04_css_react/css/src/App.jsx
+++ 04_css_react/css/src/App.jsx
@@ -2,3 +2,38 @@ import { useState } from 'react'
 import './App.css'
 import MyComponent from './components/MyComponent'
 import Title from './components/Title';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const n = 15;
+  const redTitle = false
+
+  return (
+    <>
+      <h1>React com css</h1>
+      <MyComponent />
+      {/* Inline Css */}
+      <p style={{
+        color: 'black',
+        background: 'tomato',
+        padding: '25px',
+        borderTop: '2px solid red'
+      }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      {/* css inline dinâmico */}
+      <h2 style={n < 10 ? ({color: 'purple'}) : ({color: 'pink'})}>Css dinâmico</h2>
+      <h2 style={n > 10 ? ({color: 'purple'}) : ({color: 'pink'})}>Css dinâmico</h2>
+
+      {/* Classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter classe dinâmica</h2>
+
+      {/* css modules */}
+      <Title />
+
+    </>
+  )
+}
+
+export default App
