4124210d22ccaefec412885921574d8f241aac68
Gabriel Chagas <gabrielserq@outlook.com>
2024-04-22 21:08:16 -0300

    chore: add myComponent.jsx

---
+++ 04_css_react/css/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>My Component</h1>
+        <p>Este é o paragráfo do componente</p>
+        <p className="my-comp-paragraph">teste</p>
+    </div>
+  )
+}
+
+export default MyComponent
