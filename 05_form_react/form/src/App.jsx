d44597278eedb632757a0003bcea31967191c99e
Gabriel Chagas <gabrielserq@outlook.com>
2024-04-25 21:13:20 -0300

    chore: creating const app

--- 05_form_react/form/src/App.jsx
+++ 05_form_react/form/src/App.jsx
@@ -1,3 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import MyForm from './components/MyForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Forms</h2>
+      <MyForm user={{name: "Maysa", email: "maysaomartins@teste.com", bio: 'Sou fisioterapeuta', role: 'admin'}} />
+    </>
+  )
+}
