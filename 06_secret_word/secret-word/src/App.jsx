a233b28673a19b2bf5adc4ef69223f19bc5ae78b
Gabriel Chagas <gabrielserq@outlook.com>
2024-04-27 21:15:57 -0300

    chore: creating App.jsx

---
+++ 06_secret_word/secret-word/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useCallback, useEffect } from 'react'
+
+// css
+import './App.css'
+
+// components
+import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+// data
+import { wordsList } from './data/words'
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+]
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+  
+  return (
+    <>
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
+    </>
+  )
+}
+
+export default App
