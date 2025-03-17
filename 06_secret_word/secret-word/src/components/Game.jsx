4235a0146c8478663a2f9ac9cffebd1b25c5e44b
Gabriel Chagas <gabrielserq@outlook.com>
2025-03-17 15:36:07 -0300

    :sparkles: chore: add Game.jsx

--- 06_secret_word/secret-word/src/components/Game.jsx
+++ 06_secret_word/secret-word/src/components/Game.jsx
@@ -1,12 +1,66 @@
 import './Game.css'
+import PropTypes from 'prop-types';
 
-const Game = ({verifyLetter}) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score
+  }) => {
   return (
     <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Encerrar Jogo</button>
+        <div className="game">
+          <p className="points">
+            <span>Pontuação: {score}</span>
+          </p>
+          <h1>Adivinhe a palavra:</h1>
+          <h3 className="tip">
+            Dica sobre a palavra: <span>{pickedCategory}</span>
+          </h3>
+          <p>Você ainda tem {guesses} tentativa(s).</p>
+          <div className="wordContainer">
+            {letters.map((letter, i) => (
+                guessedLetters.includes(letter) ? (
+                  <span key={i} className="letter"></span>
+                ) : (
+                  <span key={i} className="blankSquare"></span>
+                )
+              ))
+            }
+          </div>
+          <div className="letterContainer">
+            <p>Tente adivinhar uma letra da palavra:</p>
+            <form>
+              <input type="text" name='letter' maxLength="1" required />
+              <button>Jogar!</button>
+            </form>
+          </div>
+          <div className="wrongLettersContainer">
+            <p>Letras já utilizadas:</p>
+            {
+              wrongLetters.map((letter, i) => (
+                <span key={i}>{letter},</span>
+              ))
+            }
+          </div>
+        </div>
     </div>
   )
 }
+
+Game.propTypes = {
+  verifyLetter: PropTypes.func.isRequired,
+  pickedWord: PropTypes.string.isRequired,
+  pickedCategory: PropTypes.string.isRequired,
+  letters: PropTypes.arrayOf(PropTypes.string).isRequired,
+  guessedLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
+  wrongLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
+  guesses: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired
+};
 
 export default Game
