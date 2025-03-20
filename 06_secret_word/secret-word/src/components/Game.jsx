1935d05d07d1675ad9b619e866d33791fd01bb15
Gabriel Chagas <gabrielserq@outlook.com>
2025-03-20 16:37:08 -0300

    :sparkles: chore: add Game.jsx

--- 06_secret_word/secret-word/src/components/Game.jsx
+++ 06_secret_word/secret-word/src/components/Game.jsx
@@ -1,56 +1,90 @@
-import './Game.css'
-import PropTypes from 'prop-types';
+import { useRef, useState} from "react";
+import "./Game.css";
+import PropTypes from "prop-types";
 
 const Game = ({
   verifyLetter,
-  pickedWord,
   pickedCategory,
+  pickedWord,
   letters,
   guessedLetters,
   wrongLetters,
   guesses,
-  score
-  }) => {
+  score,
+  showFireworks,
+  fireworksRef
+}) => {
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+    setLetter("");
+    letterInputRef.current.focus();
+  };
+
   return (
-    <div>
-        <div className="game">
-          <p className="points">
-            <span>Pontuação: {score}</span>
-          </p>
-          <h1>Adivinhe a palavra:</h1>
-          <h3 className="tip">
-            Dica sobre a palavra: <span>{pickedCategory}</span>
-          </h3>
-          <p>Você ainda tem {guesses} tentativa(s).</p>
-          <div className="wordContainer">
-            {letters.map((letter, i) => (
-                guessedLetters.includes(letter) ? (
-                  <span key={i} className="letter"></span>
-                ) : (
-                  <span key={i} className="blankSquare"></span>
-                )
-              ))
-            }
-          </div>
-          <div className="letterContainer">
-            <p>Tente adivinhar uma letra da palavra:</p>
-            <form>
-              <input type="text" name='letter' maxLength="1" required />
-              <button>Jogar!</button>
-            </form>
-          </div>
-          <div className="wrongLettersContainer">
-            <p>Letras já utilizadas:</p>
-            {
-              wrongLetters.map((letter, i) => (
-                <span key={i}>{letter},</span>
-              ))
-            }
-          </div>
-        </div>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação</span>: {score}
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>Você ainda tem {guesses} tentativa(s).</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span className="letter" key={i}>
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        )}
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            onChange={(e) => setLetter(e.target.value)}
+            required
+            value={letter}
+            ref={letterInputRef}
+          />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
+      </div>
+
+      {/* Container dos fogos de artifício */}
+      {showFireworks && (
+        <div
+          ref={fireworksRef}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            zIndex: 999,
+          }}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
 Game.propTypes = {
   verifyLetter: PropTypes.func.isRequired,
@@ -60,7 +94,10 @@ Game.propTypes = {
   guessedLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
   wrongLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
   guesses: PropTypes.number.isRequired,
-  score: PropTypes.number.isRequired
+  score: PropTypes.number.isRequired,
+  showFireworks: PropTypes.bool.isRequired,
+  fireworksRef: PropTypes.shape({ current: PropTypes.any })
+  
 };
 
-export default Game
+export default Game;
