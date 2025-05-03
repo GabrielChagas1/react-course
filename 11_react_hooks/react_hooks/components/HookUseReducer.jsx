3301197655a306e0db072f1dd792cb0b250994da
Gabriel Chagas <gabrielserq@outlook.com>
2025-05-03 20:45:29 -0300

    :sparkles: chore: add HookUseReducer.jsx

---
+++ 11_react_hooks/react_hooks/components/HookUseReducer.jsx
@@ -0,0 +1,64 @@
+import { useReducer, useState } from "react";
+
+const HookUseReducer = () => {
+  // começando com useReducer
+  const [number, dispatch] = useReducer((state, action) => {
+    return Math.random(state)
+  })
+
+  // avançando no useReducer
+  const initialTasks = [
+    {id: 1, text: "Fazer alguma coisa"},
+    {id: 2, text: "Fazer alguma coisa"}
+  ]
+
+  const taskReducer = (state, action) => {
+    switch (action.type){
+      case "ADD": {
+        const newTask = {
+          id: Math.random(),
+          text: action.payload
+        }
+        return [...state, newTask]
+      }
+      case "DELETE": 
+        return state.filter((task) => task.id !== action.id)
+      default: 
+        return state
+        
+    }
+
+  }
+
+  const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks)
+  const [taskText, setTaskText] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatchTasks({ type: "ADD", payload: taskText });
+    setTaskText("")
+  }
+
+  const removeTask = (id) => {
+    dispatchTasks({ type: "DELETE", id: id  });
+  }
+
+
+  return (
+    <div>
+      <h2>useReducer</h2>
+      <p>Número: {number}</p>
+      <button onClick={dispatch}>Alterar Número!</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setTaskText(e.target.value)} value={taskText} />
+        <input type="submit" value="Enviar" />
+      </form>
+      {tasks.map((task) => (
+        <li key={task.id} onDoubleClick={() => removeTask(task.id)}>{task.text}</li>
+      ))}
+      <hr />
+    </div>
+   );
+}
+ 
+export default HookUseReducer;
