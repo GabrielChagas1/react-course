aab90a7dd595b3eac207b1633a031ed4a9ce2834
Gabriel Chagas <gabrielserq@outlook.com>
2025-05-05 14:52:22 -0300

    sparkles: chore: add HookUseLayoutEffect.jsx

---
+++ 11_react_hooks/react_hooks/components/HookUseLayoutEffect.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useLayoutEffect, useState } from "react";
+
+const HookUseLayoutEffect = () => {
+  const [name, setName] = useState("Algum nome")
+
+  useEffect(() => {
+    console.log("2")
+    setName("Mudou de novo!")
+  }, [])
+
+  useLayoutEffect(() => {
+    console.log("1")
+    setName("Outro nome!")
+  },[])
+
+  console.log(name)
+
+  return ( 
+    <div>
+      <p>UseLayoutEffect</p>
+      <p>Nome: {name}</p>
+      <hr />
+    </div>
+   );
+}
+ 
+export default HookUseLayoutEffect;
