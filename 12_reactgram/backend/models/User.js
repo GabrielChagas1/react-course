5a0670c190686d53256be914d7f7498016c0f72c
Gabriel Chagas <gabrielserq@outlook.com>
2025-05-13 21:43:44 -0300

    :sparkles: chore: create userSchema

--- 12_reactgram/backend/models/User.js
+++ 12_reactgram/backend/models/User.js
@@ -1,2 +1,10 @@
 const mongoose = require("mongoose")
 const {Schema} = mongoose
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  bio: String
+},{timestamps: true})
