8fd4fe4ad70576690fe4e1833c69a69354d8cc21
Gabriel Chagas <gabrielserq@outlook.com>
2025-05-15 23:08:41 -0300

    :sparkles:  chore: get authHeader and token

--- 12_reactgram/backend/middlewares/authGuard.js
+++ 12_reactgram/backend/middlewares/authGuard.js
@@ -1,3 +1,6 @@
 const User = require("../models/User")
 const jwt = require("jsonwebtoken")
 const jwtSecret = process.env.JWT_SECRET
+
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split(" ")[1]
