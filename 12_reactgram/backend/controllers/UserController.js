330a5a3439c6d4f492339a1557ac8873bb1086ec
Gabriel Chagas <gabrielserq@outlook.com>
2025-05-20 09:57:45 -0300

    :sparkles: chore: recovering jwtSecret for env

--- 12_reactgram/backend/controllers/UserController.js
+++ 12_reactgram/backend/controllers/UserController.js
@@ -3,3 +3,5 @@ const User = require("../models/User")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const mongoose = require("mongoose")
+
+const jwtSecret = process.env.JWT_SECRET
