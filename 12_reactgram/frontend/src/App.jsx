c2b98c657f98a09e7d19550f7ea14c13207b6335
Gabriel Chagas <gabrielserq@outlook.com>
2025-05-31 23:14:43 -0300

    :sparkles: chore: import components  navbar and footer

--- 12_reactgram/frontend/src/App.jsx
+++ 12_reactgram/frontend/src/App.jsx
@@ -1,3 +1,8 @@
 import './App.css'
+
 // Router
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
+// Components
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
