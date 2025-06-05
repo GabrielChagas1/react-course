221e1d82bd947547ee3696a47c39ee379fe0193d
Gabriel Chagas <gabrielserq@outlook.com>
2025-06-05 07:52:02 -0300

    :sparkles: chore: import components

--- 12_reactgram/frontend/src/components/Navbar/Navbar.jsx
+++ 12_reactgram/frontend/src/components/Navbar/Navbar.jsx
@@ -1,1 +1,6 @@
 import "./Navbar.css"
+
+// components
+import { NavLink, Link, useNavigate } from "react-router-dom";
+import {BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill} from "react-icons/bs"
+
