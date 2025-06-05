97bf3e739709dd0ae1591b36f4563cfa16752013
Gabriel Chagas <gabrielserq@outlook.com>
2025-06-05 07:52:21 -0300

    :sparkles: chore: import hooks

--- 12_reactgram/frontend/src/components/Navbar/Navbar.jsx
+++ 12_reactgram/frontend/src/components/Navbar/Navbar.jsx
@@ -4,3 +4,7 @@ import "./Navbar.css"
 import { NavLink, Link, useNavigate } from "react-router-dom";
 import {BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill} from "react-icons/bs"
 
+// hooks
+import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
