d453f3af47cf8f7cc6131a9abd5f2b3472c88199
Gabriel Chagas <gabrielserq@outlook.com>
2025-07-24 19:15:51 -0300

    :sparkles: chore: add photoSlice.jsx

--- 12_reactgram/frontend/src/slices/photoSlice.jsx
+++ 12_reactgram/frontend/src/slices/photoSlice.jsx
@@ -120,6 +120,30 @@ export const comment = createAsyncThunk(
   }
 );
 
+// Get all photos
+export const getPhotos = createAsyncThunk("photo/getall", async (_, thunkAPI) => {
+  const token = thunkAPI.getState().auth.user.token
+  const data = await photoService.getPhotos(token);
+
+  return data;
+});
+
+// Search photos by title
+export const searchPhotos = createAsyncThunk(
+  "photo/search",
+  async (query, thunkAPI) => {
+
+    const token = thunkAPI.getState().auth.user.token;
+    console.log(token)
+    const data = await photoService.searchPhotos(query, token);
+
+    console.log(data);
+    console.log(data.errors);
+
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -230,19 +254,41 @@ export const photoSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     })
-     .addCase(comment.fulfilled, (state, action) => {
-        state.loading = false;
-        state.success = true;
-        state.error = null;
+    .addCase(comment.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.error = null;
 
-        state.photo.comments.push(action.payload.comment);
+      state.photo.comments.push(action.payload.comment);
 
-        state.message = action.payload.message;
-      })
-      .addCase(comment.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      })
+      state.message = action.payload.message;
+    })
+    .addCase(comment.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    })
+    .addCase(getPhotos.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    })
+    .addCase(getPhotos.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.loading = false;
+      state.success = true;
+      state.error = null;
+      state.photos = action.payload;
+    })
+    .addCase(searchPhotos.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    })
+    .addCase(searchPhotos.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.loading = false;
+      state.success = true;
+      state.error = null;
+      state.photos = action.payload;
+    });
   }
 })
 
