0d3bb76caac792ae8611e4dadf793b156076a212
Gabriel Chagas <gabrielserq@outlook.com>
2025-07-31 23:37:53 -0300

    :sparkles chore: add arrow function

--- 13_javascript_moderno/script.js
+++ 13_javascript_moderno/script.js
@@ -36,3 +36,41 @@ console.log(name)
 const sum = function sum(a, b) {
   return a + b
 }
+
+
+const arrowSum = (a, b) => a + b
+
+console.log(sum(5, 5))
+console.log(arrowSum(5, 5))
+
+const greeting = (name) => {
+  let greeting = ''
+  name ?  greeting = `Olá ${name}` : greeting = 'Olá'
+  return greeting
+}
+
+console.log(greeting('Gabriel'))
+console.log(greeting())
+
+const testeArrow = () => console.log('Testou')
+
+testeArrow()
+
+const user = {
+  name: "Gabriel",
+  sayUserName(){
+    setTimeout(function() {
+      console.log(this)
+      console.log(`Username: ${this.name}`)
+    }, 500)
+  },
+  sayUserNameArrow(){
+    setTimeout(() => {
+      console.log(this)
+      console.log(`Username: ${this.name}`)
+    }, 700)
+  }
+}
+
+// user.sayUserName();
+// user.sayUserNameArrow();
