ba04bfcd39a7abc66db413da2c59147d6ca836d1
Gabriel Chagas <gabrielserq@outlook.com>
2025-08-09 22:11:34 -0300

    :sparkles: chore: add App.tsx

---
+++ 17_todo_react/todo_react_ts/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import styles from './App.module.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import TaskForm from './components/TaskForm/TaskForm';
+import TaskList from './components/TaskList/TaskList';
+import { ITask } from './interfaces/Task';
+
+function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
+  return (
+    <>
+      <Header/>
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer ?</h2>
+          <TaskForm btnText="Criar Tarefa" taskList={taskList} setTaskList={setTaskList} />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
