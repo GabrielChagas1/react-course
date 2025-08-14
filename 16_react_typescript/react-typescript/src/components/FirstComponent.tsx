79308bd2144a9bcef2ff440ad59fe9b54aded0da
Gabriel Chagas <gabrielserq@outlook.com>
2025-08-14 16:22:35 -0300

    :sparkles: chore: add FirstComponent.tsx

---
+++ 16_react_typescript/react-typescript/src/components/FirstComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+function FirstComponent(): React.ReactElement {
+  return (
+    <div>
+      <h1>Meu primeiro componente</h1>
+    </div>
+  )
+};
+
+export default FirstComponent;
