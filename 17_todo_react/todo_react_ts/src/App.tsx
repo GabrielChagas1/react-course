7a7bd82bc3b0da8b22d0cbbdd3d3e95debe94313
Gabriel Chagas <gabrielserq@outlook.com>
2025-08-21 23:12:12 -0300

    :sparkles: chore: add App.tsx

--- 17_todo_react/todo_react_ts/src/App.tsx
+++ 17_todo_react/todo_react_ts/src/App.tsx
@@ -4,15 +4,61 @@ import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import TaskForm from './components/TaskForm/TaskForm';
 import TaskList from './components/TaskList/TaskList';
-import { ITask } from './interfaces/Task';
+import type { ITask } from './interfaces/Task';
+import Modal from './components/Modal/Modal';
 
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([])
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter(task => {
+        return task.id !== id
+      })
+    )
+  }
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.getElementById("modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+    hideOrShowModal(false);
+  };
+
 
   return (
     <>
-      <Header/>
+      <Modal
+        title="Editar tarefa"
+        children={
+          <TaskForm
+            btnText="Editar"
+            taskList={taskList}
+            task={taskToUpdate}
+            handleUpdate={updateTask}
+          />
+        }
+      />      <Header/>
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer ?</h2>
@@ -20,7 +66,7 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList />
+          <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask} />
         </div>
       </main>
       <Footer />
